65272611268d5e245b6f7c674e75d8f71b014b5c
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-08-29 22:22:54 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import Map from './components/Map'
+import './App.css'
+import "leaflet/dist/leaflet.css";
+
+function App() {
+  return (
+     <div className="container">
+      <table>
+        <thead>
+          <tr>test row</tr>
+          <tr>test row</tr>
+        </thead>
+      </table>
+      <Map/>
+     </div>
+  )
+}
+
+export default App
