59247430af542cb0640171ef141313004933f353
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-09-02 21:01:56 +0200

    added a small footer

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <h1> 🏔️open <span className='text-backg'><span className='text-backg-span'>BoulderMap </span></span>🏔️</h1>
     <App />
-    <span></span>
-  </StrictMode>
+    <section id="footer">
+    <span>Please feel free to contribute to this project on <a href="https://github.com/nils-dambowy/openbouldermap" target="_blank" rel="noreferrer">GitHub</a></span>
+    </section>
+    </StrictMode>
 )
