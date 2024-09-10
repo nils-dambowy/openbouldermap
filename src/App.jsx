94aeb64955a5e1fd52a4cc63c31481c5d35a16fd
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-09-10 18:41:33 +0200

    updated css and removed navbar
    removed unnecessary css code and overwrote the bootstrap container class to make the map, removed the navigation since it has no functionality right now and it is not sure if it will be included in the final map

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import Map from './components/Map';
 import './App.css';
 import "../node_modules/leaflet-geosearch/dist/geosearch.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DropdownMenu from './components/DropdownMenu';
 
 function App() {
   return (
      <div className="container">
-      <div className="navbar-container">
-      <DropdownMenu/>
-      </div>
       <Map/>
       
      </div>
