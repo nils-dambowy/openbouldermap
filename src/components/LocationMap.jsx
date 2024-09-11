b9f433fb7e1c3bffd2b2e752ce7f3c97a11814fd
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-09-11 22:27:24 +0200

    Update LocationMap.jsx
    fixed the issue that the Location button had to be clicked twice, removed unnecessary code

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -1,31 +1,18 @@
-import { useState } from 'react';
-import { useMapEvents } from 'react-leaflet';
+import { useMap } from 'react-leaflet';
 import "../App.css";
 
 export default function LocationMarker() {
-    const [position, setPosition] = useState(null)
-    const map = useMapEvents({
-        click() {
-          map.locate()
-          //const { lat, lng } = e.latlng;
-          // this allows adds another marker to the map
-          // however, it works without the need of the component
-          // --> I still need to revise t
-          //L.marker([lat, lng], { icon }).addTo(map);
-        },
-        locationfound(e) {
-            setPosition(e.latlng)
-            //map.flyTo(e.latlng, map.getZoom())
-          },
-      })
+    const map = useMap();
 
     return(
         <button
         type="button"
         className="edit-location-button"
         onClick={() => {
-            map.flyTo(position);
-            }}
+          map.locate().on("locationfound", function (e) {
+            map.flyTo(e.latlng, map.getZoom());
+          });
+        }}
          >
         Set to current location
       </button>
