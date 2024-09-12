be413c8bca4749ff2f541be546b09bc10396753e
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-09-12 19:00:57 +0200

    fixed button click propagation error
    - when clicking the "Set to current location" button click-events where propagated to parents, which in this case is the map and marker was added to the map.
    
    L.DomEvent.disableClickPropagation(btnRef.current);
    
    This prevents the bubbling up of events.

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -1,18 +1,31 @@
-import { useMap } from 'react-leaflet';
+import { useMap} from 'react-leaflet';
+import { useEffect, useRef } from 'react';
 import "../App.css";
+import L from "leaflet";
 
 export default function LocationMarker() {
+   const btnRef = useRef(null);
     const map = useMap();
+
+    /* prevents the button from triggering the map click event */
+    useEffect(() => {
+      L.DomEvent.disableClickPropagation(btnRef.current);
+    });
+
+    const handleButtonClick = (e) => {
+      e.stopPropagation(); 
+      L.DomEvent.stopPropagation(e);
+      map.locate().on("locationfound", function (e) {
+        map.flyTo(e.latlng, map.getZoom());
+      });
+    };
 
     return(
         <button
         type="button"
+        ref={btnRef}
         className="edit-location-button"
-        onClick={() => {
-          map.locate().on("locationfound", function (e) {
-            map.flyTo(e.latlng, map.getZoom());
-          });
-        }}
+        onClick={handleButtonClick}
          >
         Set to current location
       </button>
