64426c214ffc04ed5c2fbed110bf2331ca6b9cb9
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-10-05 14:24:32 +0200

    Create services.js

---
+++ frontend/src/util/services.js
@@ -0,0 +1,17 @@
+const backendUrl = "cgregarious-exploration"
+
+async function fetchMovies() {
+    try {
+        const response = await fetch(`${backendUrl}/movies`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+        throw error;
+    }
+}
+
+fetchMovies();
