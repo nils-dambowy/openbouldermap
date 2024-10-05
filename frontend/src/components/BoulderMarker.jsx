8d39ddf76c25977e77537927dd3caf5546996d19
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-10-05 15:46:46 +0200

    Update BoulderMarker.jsx

--- frontend/src/components/BoulderMarker.jsx
+++ frontend/src/components/BoulderMarker.jsx
@@ -1,30 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useMapEvents, Marker, Popup } from 'react-leaflet';
 import { uuidv7 } from "uuidv7";
 import { fetchBoulders } from '../util/services';
 
-/* 
-Disclamer: This is a edited copy of the code from the React Leaflet documentation.  
-*/
-
-// for pre-configured markers
-//const marker_arr = await getBoulderLocations();
-const marker_arr = [];
-
-const getMovies = async () => {
-  try {
-      const boulderData = await fetchBoulders();
-      marker_arr.push(...boulderData);
-      console.log(marker_arr);
-  } catch (error) {
-      console.error('Error fetching movies:', error);
-  }
-};
-
-getMovies();
-
 export default function BoulderMarker() {
   const [, setPosition] = useState(null);
+  const [markerArr, setMarkerArr] = useState([]);
+
+  useEffect(() => {
+    const getBoulders = async () => {
+      try {
+        const boulderData = await fetchBoulders(); 
+        setMarkerArr(boulderData); 
+        console.log('Fetched boulders:', boulderData);
+      } catch (error) {
+        console.error('Error fetching boulders:', error);
+      }
+    };
+
+    getBoulders(); 
+  }, []); 
+
   
   useMapEvents({
     click(e) {
@@ -33,9 +29,9 @@ export default function BoulderMarker() {
     },
   })
 
-  return marker_arr.map((pos) =>{
+  return markerArr.map((pos) =>{
     console.log("Entering map");
-    console.log("size of marker_arr: ", marker_arr.length);
+    console.log("size of marker_arr: ", markerArr.length);
     const identifier = uuidv7();
     const latlng = {"lat": pos.lat, "lng": pos.lng};
     console.log(latlng);
