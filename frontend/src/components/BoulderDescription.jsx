b717fec3cc4be3eb53edb47325ccd889bff46a92
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-10-16 19:44:48 +0200

    style: added first styled component
    Installed styled component package to the frontend and added refactored the BoulderDescription to a styled component

--- frontend/src/components/BoulderDescription.jsx
+++ frontend/src/components/BoulderDescription.jsx
@@ -1,12 +1,19 @@
 import BoulderIcon from '../assets/boulder-icon.png';
+import styled from 'styled-components';
 import "../App.css";
 
+const StyledImg = styled.img`
+  width: 50px;
+  height: 50px;
+  margin: auto;
+  display: block;
+`;
 
 export default function BoulderDescription({name, description, difficulty}) {
   return (
-    <div className="BoulderDescription">
+    <div>
       <h1>{name}</h1>
-      <img src={BoulderIcon} alt="boulder-icon" className="boulder-icon" />
+      <StyledImg src={BoulderIcon} alt="boulder-icon" />
       <p>Description: {description}</p>
       <p>Difficulty: {difficulty}</p>
     </div>
